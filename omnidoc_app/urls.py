76f5891abbc57ca609534c7087e4ff32413b6767
Vihaan Vulpala <vihaanv14@gmail.com>
2024-10-19 15:19:00 -0400

    added front end for doctors

--- omnidoc_app/urls.py
+++ omnidoc_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main_view
+from .views import main_view, doctor_patient_list_view
 
 urlpatterns = [
     path('main/', main_view, name='main'),  # URL is handled in the app-level urls.py
+    path('doctor/', doctor_patient_list_view, name='doctor_patient_list')
 ]
--- omnidoc_app/views.py
+++ omnidoc_app/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import subprocess
 
+from omnidoc_app.models import Patient
+
+
 def main_view(request):
     return render(request, 'main.html')  
 
@@ -14,3 +17,11 @@ def run_python_script(request):
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=500)
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+
+def doctor_patient_list_view(request):
+    # Query all Patient models
+    patients = Patient.objects.all()
+
+    # Render the template and pass the patient list to the context
+    return render(request, 'patient_list.html', {'patients': patients})
