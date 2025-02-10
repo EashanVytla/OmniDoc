225832530766bde17895e9e9f91773c2439c58d0
Vulpala <vulpala.1@MacBook-Pro-9.local>
2025-02-10 17:19:00 -0500

    changed chat session URLs to be associated with session id

--- omnidoc_app/urls.py
+++ omnidoc_app/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import main_view, doctor_patient_list_view, doctor_session_view, doctor_session_detail, start_recording
 
 urlpatterns = [
-    path('main/<uuid:patient_id>/', main_view, name='main'),  # URL is handled in the app-level urls.py
+    path('main/', main_view, name='main'),
+    path('main/<uuid:session_id>/', main_view, name='main'),  # URL is handled in the app-level urls.py
     path('doctor/', doctor_patient_list_view, name='doctor_patient_list'),
-    path('start-recording/<uuid:patient_id>/', start_recording, name='start_recording'),
+    path('start-recording/<uuid:session_id>/', start_recording, name='start_recording'),
     path('doctor/<uuid:patient_id>/', doctor_session_view, name='doctor_session_list'),
     path('doctor/<uuid:patient_id>/<uuid:session_id>/', doctor_session_detail, name='doctor_session_detail'),
     path('receive-data/', start_recording, name='receive_data'),
--- omnidoc_app/voice_control/voice_to_wav.py
+++ omnidoc_app/voice_control/voice_to_wav.py
@@ -1,3 +1,5 @@
+import os 
+import sys
 import pyaudio
 import wave
 import webrtcvad
